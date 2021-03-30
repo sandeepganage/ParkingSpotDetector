cc358b00a7de931de8c5ff51fdb404870617dcd2
unknown <sandeep.ganage@yahoo.in>
2021-03-30 14:15:46 +0530

    code cleaning sample test

--- Flask/Server.py
+++ Flask/Server.py
@@ -6,17 +6,6 @@ import os
 
 global config
 
-
-
-# class MyFlaskApp(Flask):
-#     def run(self, host=None, port=None, debug=None, load_dotenv=True, **options):
-#         if not self.debug or os.getenv('WERKZEUG_RUN_MAIN') == 'true':
-#             with self.app_context():
-#                 init()
-#         super(MyFlaskApp, self).run(host=host, port=port, debug=debug, load_dotenv=load_dotenv, **options)
-
-
-# app = MyFlaskApp(__name__)
 app = Flask(__name__)
 
 
