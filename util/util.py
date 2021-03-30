9039f3f762736be6982808a22865b8d6d153b6f4
sandeepganage <sandeep.ganage@yahoo.in>
2021-03-30 22:21:07 +0530

    parking spots added for cam2. Only ref images created for both parking spots and globals masks. Backup masks files deleted.

--- util/util.py
+++ util/util.py
@@ -1,10 +1,10 @@
 
-DIR = "C:/PSD/"
-DIR_DATA = "C:/PSD/data/"
+DIR = "C:/ParkingSpotDetector/"
+DIR_DATA = "C:/ParkingSpotDetector/data/"
 userID = 'admin'
 password = 'CPPJNPT@123'
 available_cams = {1: "rtsp://192.168.1.110:554/user=admin_password=oyXv12aW_channel=1_stream=0.sdp?real_stream",
                   2: "rtsp://192.168.1.109:554/user=admin_password=oyXv12aW_channel=1_stream=0.sdp?real_stream"}
 cam_dimension = {1: (1296, 2304), 2: (1296, 2304)}
-available_model = {1: 'Model_ep0125_loss0.0772_Cam_1_2V.h5'}
+available_model = {1: 'PSD_Model_ep0125_loss0.0772.h5'}
 decision_threshold = 20
